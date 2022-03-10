9ec4f864b6ee9af4b09bc88364f16ad11e5aa5ca
myehin <78196331+myehin@users.noreply.github.com>
2022-03-10 19:57:03 +0900

    [2022년 3월 10일 - 조혜민] 프로젝트 생성 및 톰캣 서버 설정 - JDK1.8 - Tomcat9 - Spring Maven

---
+++ idemo20220310/src/main/java/com/example/demo/Idemo20220310Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idemo20220310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idemo20220310Application.class, args);
+	}
+
+}
